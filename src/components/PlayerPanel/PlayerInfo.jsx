6aa97d25c413518acaf65eac13d08797335b0ce6
computerbusan-bit <computer.busan@gmail.com>
2026-01-04 21:25:57 +0900

    코드 추가 및 수정
    코드 추가 및 수정

--- src/components/PlayerPanel/PlayerInfo.jsx
+++ src/components/PlayerPanel/PlayerInfo.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
-export default function PlayerInfo({ isMe, playerClass, resources=[], devCards=[], roads=[], useKnight }) {
+export default function PlayerInfo({ 
+    isMe, 
+    playerClass, 
+    resources=[],
+    devCards=[], 
+    roads=[], 
+    useKnight,
+    isCurrent,
+}) {
 
     // 배열인지 확인, 아니면 빈 배열로 처리
     const resourceCount = Array.isArray(resources) ? resources.reduce((sum, c) => sum + c, 0) : 0;
@@ -10,7 +18,7 @@ export default function PlayerInfo({ isMe, playerClass, resources=[], devCards=[
     const bridgeCount = Array.isArray(roads) ? roads.length : 0;
 
     return (
-        <div className={`player ${playerClass} ${isMe ? "myProfile" : ""}`}>
+        <div className={`player playerInfoCard ${playerClass} ${isMe ? "myProfile" : ""} ${isCurrent ? "active" : ""}`}>
             <div className="icon">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4">
                     <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
--- src/components/PlayerPanel/PlayerPanel.jsx
+++ src/components/PlayerPanel/PlayerPanel.jsx
@@ -1,17 +1,41 @@
 // 플레이어의 자원, 개발 카드, 점수 등을 표시하는 패널 UI입니다.
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import "../../styles/PlayerPanel.css";
 import Bank from "./Bank";
 import PlayerInfo from "./PlayerInfo";
-import useGameStore from "../../features/state/gameStore";
+import useGameStore, {gameLog} from "../../features/state/gameStore";
 
 export default function PlayerPanel({ players }) {
 
     const currentPlayerIndex = useGameStore((state) => state.currentPlayerIndex);
 
+    //1. gameLog스토어에서 로그 배열 가져오기
+    const logs = gameLog((state) => state.log);
+    //2.스크롤을 제어하기 위한 Ref생성
+    const logContainerRef = useRef(null);
+    //3. 로그가 업데이트 될 때마다 스크롤을 맨 아래로 이동
+    useEffect(() => {
+        if(logContainerRef.current) {
+            logContainerRef.current.scrollTop = logContainerRef.current.scrollHeight;
+        }
+    }, [logs]);
     return (
         <section className="playerPanel">
-            <div className="log"></div>
+            {/* 4.ref연결 및 로그 데이터 매핑 */}
+            <div className="log" ref={logContainerRef}>
+                {logs.length === 0 ? (
+                    <div style={{color: 'white', textAlign: 'center', marginTop: '10px'}}>
+                        게임 시작 대기 중...
+                    </div>
+                ) : (
+                    logs.map((message, index) => (
+                        //key는 고유해야하니까.. 텍스트 로그니까... 인덱스로 쓸게요...
+                        <div key={index} style={{marginBottom: '4px'}}>
+                            {message}
+                        </div>
+                    ))
+                )}
+            </div>
 
             <div className="otherPlayers">
                 <Bank />
@@ -35,7 +59,9 @@ export default function PlayerPanel({ players }) {
                 resources={players[0]?.resources || 0}
                 devCards={players[0]?.devCards || 0}
                 roads={players[0]?.roads || 0}
-                useKnight={players[0]?.useKnight || 0} />
+                useKnight={players[0]?.useKnight || 0}
+                isCurrent={currentPlayerIndex === 0}
+            />
         </section>
     );
 }
